31ec49a7f28e690f4ad52bfc6e6fbb83b9cfbd0b
Tobias Westergaard Kjeldsen <tobias@wkjeldsen.dk>
2018-07-23 15:04:26 +0200

    Added donation option.

--- lib/main_page.dart
+++ lib/main_page.dart
@@ -8,28 +8,52 @@ import 'package:carwingsflutter/util.dart';
 import 'package:carwingsflutter/vehicle_page.dart';
 import 'package:dartcarwings/dartcarwings.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_payments/flutter_payments.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 class MainPage extends StatefulWidget {
-  MainPage({Key key, this.session}) : super(key: key);
+  MainPage(this.session);
 
   final CarwingsSession session;
 
   @override
-  _MainPageState createState() =>  _MainPageState(session);
+  _MainPageState createState() => _MainPageState(session);
 }
 
 class _MainPageState extends State<MainPage> {
-  final GlobalKey<ScaffoldState> scaffoldKey =  GlobalKey<ScaffoldState>();
+  final GlobalKey<ScaffoldState> scaffoldKey = GlobalKey<ScaffoldState>();
+
+  static final String SKU_DONATE = 'donate';
 
   CarwingsSession _session;
 
-  _MainPageState(this._session);
+  bool _donated = false;
+
+  _MainPageState(this._session) {
+    _donationMadeCheck();
+  }
+
+  _donationMadeCheck() async {
+    List<Purchase> purchases =
+        await FlutterPayments.getPurchaseHistory(ProductType.InApp);
+    bool donated = false;
+    if (purchases != null) {
+      for (Purchase purchase in purchases) {
+        if (purchase.sku == SKU_DONATE) {
+          donated = true;
+        }
+      }
+    }
+    setState(() {
+      _donated = donated;
+    });
+  }
 
   _locateVehicleGoogleMaps() {
     Util.showLoadingDialog(context, ('Locating vehicle..'));
     _session.getVehicle().requestLocation().then((location) {
-      launch('https://www.google.com/maps/search/?api=1&query=${location.latitude},${location.longitude}');
+      launch('https://www.google.com/maps/search/?api=1&query=${location
+          .latitude},${location.longitude}');
       Util.dismissLoadingDialog(context);
     });
   }
@@ -65,7 +89,7 @@ class _MainPageState extends State<MainPage> {
   _logOut() {
     Navigator.of(context).pushReplacement(new MaterialPageRoute<Null>(
       builder: (BuildContext context) {
-        return new LoginPage(_session);
+        return new LoginPage(_session, false);
       },
     ));
   }
@@ -77,16 +101,16 @@ class _MainPageState extends State<MainPage> {
           new DrawerHeader(
               child: new Center(
                   child: new Column(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    crossAxisAlignment: CrossAxisAlignment.center,
-                    children: <Widget>[
-                      new ImageIcon(
-                        new AssetImage('images/car-leaf.png'),
-                        size: 100.0,
-                        color: Theme.of(context).primaryColor,
-                      ),
-                    ],
-                  ))),
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: <Widget>[
+              new ImageIcon(
+                new AssetImage('images/car-leaf.png'),
+                size: 100.0,
+                color: Theme.of(context).primaryColor,
+              ),
+            ],
+          ))),
           _buildVehicleListTiles(context),
           new ListTile(
             leading: const Icon(Icons.map),
@@ -103,7 +127,8 @@ class _MainPageState extends State<MainPage> {
             leading: const Icon(Icons.exit_to_app),
             title: const Text('Log out'),
             onTap: _logOut,
-          )
+          ),
+          _donated ? Row() : _buildDonateListTile(context)
         ],
       ),
     );
@@ -111,35 +136,67 @@ class _MainPageState extends State<MainPage> {
 
   Column _buildVehicleListTiles(context) {
     List<ListTile> accountListTiles = new List<ListTile>();
-      accountListTiles.add(new ListTile(
-        leading: new ImageIcon(new AssetImage('images/sports-car.png')),
-        title: new Text(_session.getVehicle().nickname),
-        onTap: () => _openVehicleInfoPage(),
-        onLongPress: () => null,
-      ));
+    accountListTiles.add(new ListTile(
+      leading: new ImageIcon(new AssetImage('images/sports-car.png')),
+      title: new Text(_session.getVehicle().nickname),
+      onTap: () => _openVehicleInfoPage(),
+      onLongPress: () => null,
+    ));
     return new Column(children: accountListTiles);
   }
 
+  Widget _buildDonateListTile(BuildContext context) {
+    return new ListTile(
+      onTap: () async {
+        try {
+          final List<Purchase> list = await FlutterPayments.purchase(
+            sku: SKU_DONATE,
+            type: ProductType.InApp,
+          );
+
+          scaffoldKey.currentState.showSnackBar(new SnackBar(
+              duration: new Duration(seconds: 5),
+              content: new Text('Thank you for the donation!')));
+
+          _donationMadeCheck();
+        } on FlutterPaymentsException catch (error) {
+          scaffoldKey.currentState.showSnackBar(new SnackBar(
+              duration: new Duration(seconds: 5),
+              content: new Text('Too bad, donation failed!')));
+        }
+      },
+      leading: const Icon(Icons.monetization_on),
+      title: Text('Donate'),
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
-    return  Scaffold(
+    return Scaffold(
+      key: scaffoldKey,
       backgroundColor: Theme.of(context).primaryColor,
-      appBar:  AppBar(
-        title:  new Text('Dashboard'),actions: [
+      appBar: AppBar(title: new Text('Dashboard'), actions: [
         new IconButton(
-            icon: new ImageIcon(AssetImage('images/aircondition.png'), color: Colors.white),
+            icon: new ImageIcon(AssetImage('images/aircondition.png'),
+                color: Colors.white),
             onPressed: _openClimateControlPage),
         new IconButton(
             icon: new Icon(Icons.power, color: Colors.white),
             onPressed: _openChargingPage),
-      ]
+      ]),
+      body: ListView(
+        children: <Widget>[
+          new BatteryLatestCard(
+            _session,
+          ),
+          new StatisticsDailyCard(
+            _session,
+          ),
+          new StatisticsMonthlyCard(
+            _session,
+          ),
+        ],
       ),
-      body:  ListView(children: <Widget>[
-        new BatteryLatestCard(session: _session,),
-        new StatisticsDailyCard(session: _session,),
-        new StatisticsMonthlyCard(session: _session,),
-      ],),
       drawer: _buildDrawer(context),
     );
   }
