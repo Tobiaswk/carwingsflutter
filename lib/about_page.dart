be3f18b19c8fece1357a029faad53ad27114587f
Tobias Westergaard Kjeldsen <tobias@wkjeldsen.dk>
2018-12-07 20:44:20 +0100

    Fixed wording for license under about page

--- lib/about_page.dart
+++ lib/about_page.dart
@@ -5,7 +5,7 @@ import 'package:url_launcher/url_launcher.dart';
 
 class _AboutPageState extends State<AboutPage> {
   static var _LICENSE =
-      'The MIT License\n\nCopyright (c) 2018 Tobias Westergaard Kjeldsen\n\nPermission is hereby granted, free of charge, to any person obtaining a copyof this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included inall copies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS ORIMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THEAUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHERLIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS INTHE SOFTWARE.';
+      'The MIT License\n\nCopyright (c) 2018 Tobias Westergaard Kjeldsen\n\nPermission is hereby granted, free of charge, to any person obtaining a copyof this software and associated documentation files (the "Software"), to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:\n\nThe above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.\n\nTHE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS ORIMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NON INFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS INTHE SOFTWARE.';
 
   Future _launchUrl(url) async {
     if (await canLaunch(url)) {
@@ -24,7 +24,7 @@ class _AboutPageState extends State<AboutPage> {
           new Column(
             mainAxisSize: MainAxisSize.min,
             children: <Widget>[
-              new Padding(padding: const EdgeInsets.all(10.0)),
+              new Padding(padding: const EdgeInsets.all(8.0)),
               ImageIcon(
                 AssetImage('images/car-leaf.png'),
                 color: Util.primaryColor(context),
