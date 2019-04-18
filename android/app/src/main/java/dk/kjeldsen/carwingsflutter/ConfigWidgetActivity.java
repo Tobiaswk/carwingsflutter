ab2474da8403832df023c91cdc9c9b27dfb809b8
Tobias Westergaard Kjeldsen <tobias@wkjeldsen.dk>
2019-04-18 13:50:54 +0200

    Upgrade dependencies to latest release versions Update tooling; gradle and androidx

--- android/app/src/main/java/dk/kjeldsen/carwingsflutter/ConfigWidgetActivity.java
+++ android/app/src/main/java/dk/kjeldsen/carwingsflutter/ConfigWidgetActivity.java
@@ -9,7 +9,7 @@ import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.AsyncTask;
 import android.os.Bundle;
-import android.support.annotation.Nullable;
+import androidx.annotation.Nullable;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 import android.widget.TextView;
