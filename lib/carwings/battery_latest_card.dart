84ee6c405ab0e405599a286a6dffc370e9dfc721
Tobias Westergaard Kjeldsen <tobias@wkjeldsen.dk>
2021-05-23 22:13:17 +0200

    Fix old style 12th bar notation battery card

--- lib/carwings/battery_latest_card.dart
+++ lib/carwings/battery_latest_card.dart
@@ -196,7 +196,7 @@ class _BatteryLatestCardState extends State<BatteryLatestCard> {
   @override
   Widget build(BuildContext context) {
     return Container(
-        padding: const EdgeInsets.fromLTRB(15.0, 10.0, 15.0, 10.0),
+        padding: const EdgeInsets.fromLTRB(15.0, 20.0, 15.0, 10.0),
         child: _battery != null
             ? _withValues(
                 _battery!.dateTime,
