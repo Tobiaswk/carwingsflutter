2237ce5943e1486d4d5490fbbddbafbc321020f2
Tobias Westergaard Kjeldsen <twk@lessor.dk>
2024-11-17 22:00:15 +0100

    HelpPage
    Formatting and wording

--- lib/help_page.dart
+++ lib/help_page.dart
@@ -1,4 +1,6 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class _HelpPageState extends State<HelpPage> {
   @override
@@ -14,18 +16,21 @@ class _HelpPageState extends State<HelpPage> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: <Widget>[
                   Padding(padding: const EdgeInsets.all(5.0)),
-                  Text('Having problems signing in? Follow these steps below'),
+                  Text(
+                    'Having problems signing in? Follow these steps below',
+                    style: TextStyle(fontSize: 20.0),
+                  ),
                   Padding(padding: const EdgeInsets.all(5.0)),
                   Text('Setup / Before you use',
                       style: TextStyle(fontSize: 18.0)),
                   Text(
-                      'Remember to first complete the initial sign up process of your NissanConnect account and vehicle with the official app from Nissan before trying My Leaf'),
+                      '1. Remember to first complete the initial sign up process of your NissanConnect account and vehicle with the official app from Nissan before trying My Leaf'),
                   Padding(padding: const EdgeInsets.all(5.0)),
                   Text(
-                      'Remember that My Leaf is directly affected by Nissan\'s (sometimes poor) infrastructure! If the services/official app are down/unstable so is My Leaf'),
+                      '2. Remember that My Leaf is directly affected by Nissan\'s infrastructure! If the services/official app are down/unstable so is My Leaf'),
                   Padding(padding: const EdgeInsets.all(5.0)),
                   Text(
-                      'To use My Leaf a NissanConnect subscription from Nissan is required'),
+                      '3. To use My Leaf a NissanConnect subscription from Nissan is required'),
                   Padding(padding: const EdgeInsets.all(5.0)),
                   Text('Vehicles produced after May 2019',
                       style: TextStyle(fontSize: 18.0)),
@@ -34,11 +39,34 @@ class _HelpPageState extends State<HelpPage> {
                   Padding(padding: const EdgeInsets.all(5.0)),
                   Text('Turn on debugging', style: TextStyle(fontSize: 18.0)),
                   Text(
-                      'Hold your finger on the My Leaf icon; preferences will open. Press \'Turn on debugging\'. Try signing in again. Hold your finger on the My Leaf icon again; enter \'Debug log\'. Press the clipboard icon in the top right corner'),
+                      '1. Hold your finger on the My Leaf icon; preferences will open'),
+                  Text('2. Press \'Turn on debugging\''),
+                  Text('3. Try signing in again'),
+                  Text(
+                      '4. Hold your finger on the My Leaf icon again; enter \'Debug log\''),
+                  Text('5. Press the clipboard icon in the top right corner'),
                   Padding(padding: const EdgeInsets.all(5.0)),
                   Text('Direct contact', style: TextStyle(fontSize: 18.0)),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      TextButton(
+                          onPressed: () =>
+                              launchUrl(Uri.parse('mailto:me@tobis.dk')),
+                          child: Text('Send an email')),
+                      IconButton(
+                        icon: Icon(Icons.copy),
+                        onPressed: () {
+                          Clipboard.setData(ClipboardData(text: 'me@tobis.dk'));
+                          ScaffoldMessenger.of(context).showSnackBar(SnackBar(
+                            content: Text("Email copied"),
+                          ));
+                        },
+                      )
+                    ],
+                  ),
                   Text(
-                      'Use contact information from the store listing for this app. Include the debug log from above step. Please do this instead of defaulting to a harmful 1 star review '),
+                      'Include the debugging information from above step. Please do this instead of turning to a 1-star review '),
                 ],
               ))
         ],
